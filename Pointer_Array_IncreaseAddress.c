fb4854f377ab3aa7189415cfc5b779ac4508fd6b
Hồ Đăng Tú <113624811+hodangtu01@users.noreply.github.com>
2024-07-31 21:30:37 +0700

    Add files via upload

---
+++ Pointer_Array_IncreaseAddress.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+void inmang(int *mang, int sophantu){
+    for(int i = 0; i < sophantu; i++){
+        printf("Mang[%d] = %d \n", i, *mang);
+        mang++;
+        printf("Address Pointer = %x\n", mang);
+    }
+}
+
+int main(){
+    inmang(arr, 10);
+    
+    return 0;
+}
